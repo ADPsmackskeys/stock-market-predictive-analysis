64ca55df1c6a4d43e82b5b28f0b3e0b506392603
madhurasrishti <madhura.srishti2022@vitstudent.ac.in>
2025-09-14 18:12:49 +0530

    Added Models for Technical Analysis with Reports

--- models/catboost_model.py
+++ models/catboost_model.py
@@ -1,19 +1,24 @@
+# catboost_model.py
 from catboost import CatBoostClassifier
-from models.baseline_unimodal import BaseModel
+from models.base_model import BaseModel
+import pandas as pd
 
 class CatBoostModel(BaseModel):
     def build_model(self):
+        self.label_map = {"Decrease": 0, "Neutral": 1, "Increase": 2}
         self.model = CatBoostClassifier(
             loss_function="MultiClass",
             iterations=500,
             depth=6,
             learning_rate=0.05,
-            verbose=False,
-            train_dir=None 
+            verbose=False
         )
 
     def train(self, X_train, y_train):
-        self.model.fit(X_train, y_train)
+        y_encoded = y_train.map(self.label_map)
+        self.model.fit(X_train, y_encoded)
 
     def predict(self, X_test):
-        return self.model.predict(X_test)
+        y_pred = self.model.predict(X_test)
+        inv_map = {v: k for k, v in self.label_map.items()}
+        return pd.Series(y_pred.ravel()).map(inv_map)
