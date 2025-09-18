fc523a3eb63d291b5b00991761d38057af70b1d5
madhurasrishti <madhura.srishti2022@vitstudent.ac.in>
2025-09-18 21:21:40 +0530

    Moved Folders

---
+++ models/unimodal/random_forest_model.py
@@ -0,0 +1,16 @@
+from sklearn.ensemble import RandomForestClassifier
+from models.unimodal.base_model import BaseModel
+
+class RandomForestModel(BaseModel):
+    def build_model(self):
+        self.model = RandomForestClassifier(
+            n_estimators=200,
+            max_depth=6,
+            random_state=42
+        )
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
