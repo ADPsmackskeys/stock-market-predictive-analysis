d96fd6a7b58d9a5370173d97aa263df9cd3214a9
ADPSmacksKeys <achyuth.paddy@gmail.com>
2025-10-16 22:10:06 +0530

    labeled the 1k records

--- data/news_sentiment/check_duplicate.py
+++ data/news_sentiment/check_duplicate.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 
 sys.path.insert(1, './config')
-from config.tickers import stocks
+from tickers import stocks
 
 # --- Load CSV ---
 csv_path = "data/news_sentiment/stocks_news_new.csv"
@@ -11,7 +11,7 @@ df = pd.read_csv(csv_path)
 unique_col1 = df['Date'].unique()
 print("Unique values in 'col1':")
 print(unique_col1)
-'''
+
 def get_symbol(company_name):
     if pd.isna(company_name):
         return None
@@ -30,9 +30,8 @@ print (df[df.isna ().any (axis = 1)])
 df.to_csv(csv_path, index=False)
 
 # --- Report ---
-unmatched = df[df["Symbol"].isna()]["Company Name"].unique()
+unmatched = df[df["Symbol"].isna()]["Company"].unique()
 print(f"Updated file saved: {csv_path}")
 print(f"Unmatched companies: {len(unmatched)}")
 if len(unmatched) > 0:
     print("Some unmatched names:", unmatched)
-'''
